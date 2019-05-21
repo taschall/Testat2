94c3321deacded763b90e181dab20c86411f0cc5
taschall <36793396+taschall@users.noreply.github.com>
2019-05-21 18:50:17 +0200

    added Code to RobotFahrbefehlServer

--- RobotFahrbefehlServer/Program.cs
+++ RobotFahrbefehlServer/Program.cs
@@ -2,6 +2,9 @@
 using System.Linq;
 using System.Collections.Generic;
 using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace RobotFahrbefehlServer
 {
@@ -13,7 +16,15 @@ namespace RobotFahrbefehlServer
         [MTAThread]
         static void Main()
         {
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+            TcpListener listen = new TcpListener(IPAddress.Any, 13);
+            listen.Start();
+            while (true)
+            {
+                Console.WriteLine("Warte auf Verbindung auf Port " +
+                    listen.LocalEndpoint + "...");
+                new Thread(new TimeHandler(listen.AcceptTcpClient()).Do).Start();
+            }
         }
     }
 }
