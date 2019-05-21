e8415093c016e26ac8bad27f7f160428b5e39de3
taschall <36793396+taschall@users.noreply.github.com>
2019-05-21 18:49:59 +0200

    added FahrbefehlHandler

---
+++ RobotFahrbefehlServer/FahrbefehlHandler.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net.Sockets;
+using System.IO;
+
+namespace RobotFahrbefehlServer
+{
+    class FahrbefehlHandler
+    {
+        private TcpClient client;
+
+        public FahrbefehlHandler(TcpClient client)
+        {
+            this.client = client;
+        }
+
+        public void Do()
+        {
+            Console.WriteLine("Verbindung zu " +
+                client.Client.RemoteEndPoint);
+            TextWriter tw = new StreamWriter(client.GetStream());
+            tw.Write(DateTime.Now.ToString());
+            tw.Flush();
+            client.Close();
+        }
+    }
+}
