ed708294196f0cc9104960c0f95978d7e76f9859
taschall <36793396+taschall@users.noreply.github.com>
2019-05-28 18:11:02 +0200

    implemented parser and drive

--- RobotFahrbefehlServer/FahrbefehlHandler.cs
+++ RobotFahrbefehlServer/FahrbefehlHandler.cs
@@ -38,6 +38,97 @@ namespace RobotFahrbefehlServer
             this.client = client;
         }
 
+        void ParseAndDrive()
+        {
+            String action;
+            String value1;
+            String value2 = null;
+
+            //parse Arraylist fahrbefehle
+            foreach(String element in fahrbefehle)
+            {
+                // get first fahrbefehl
+                string s = element;
+
+                // split string into sub strings
+                string[] words = s.Split(' ');
+
+                action = words[1];
+                value1 = words[2];
+
+                // Check if 3 substrings exists
+                if(words.Length >= 2)
+                {
+                    value2 = words[3];
+                }
+                else
+                {
+                    value2 = null;
+                }
+                    
+
+            //String s = dataReceived;
+            //Char charRange = ' ';
+            //int startIndex = s.IndexOf(charRange);
+            //action = s.Substring(0, startIndex);
+            //int endIndex = s.LastIndexOf(charRange);
+            //if (endIndex == startIndex) { endIndex = s.Length; }
+            //int length = endIndex - startIndex;
+            //value1 = s.Substring(startIndex + 1, length - 1);
+            //if (endIndex != s.Length)
+            //{
+            //    startIndex = endIndex;
+            //    endIndex = s.Length;
+            //    length = endIndex - startIndex;
+            //    value2 = s.Substring(startIndex + 1, length - 1);
+            //}
+
+
+            float val1 = (float)System.Convert.ToDecimal(value1);
+            int val2 = System.Convert.ToInt32(value2);
+
+            //void printit()
+            //{
+            //    Console.WriteLine(action);
+            //    Console.WriteLine("fValue1: " + val1); // Float
+            //    Console.WriteLine("iValue2: " + val2); // INTEGER
+            //}
+
+            if (action.Contains("TrackLine"))
+            {
+                Console.WriteLine("TrackLine");
+                //printit();
+            }
+            else if (action.Contains("TrackTurnLeft"))
+            {
+                Console.WriteLine("TrackTurnLeft");
+                //printit();
+            }
+            else if (action.Contains("TrackTurnRight"))
+            {
+                Console.WriteLine("TrackTurnRight");
+                //printit();
+            }
+            else if (action.Contains("TrackArcLeft"))
+            {
+                Console.WriteLine("TrackArcLeft");
+                //printit();
+            }
+            else if (action.Contains("TrackArcRight"))
+            {
+                Console.WriteLine("TrackArcRight");
+                //printit();
+            }
+            else if (action.Contains("Start"))
+            {
+                Console.WriteLine("Start");
+                //printit();
+            }
+
+          }
+
+        }
+
         public void Do()
         {
             Console.WriteLine("Verbindung zu " +
@@ -60,6 +151,10 @@ namespace RobotFahrbefehlServer
                             fahrbefehle.Add(line);
                     }
                 }
+
+                //finished receiving and start driving
+                ParseAndDrive();
+
             }
 
             finally
